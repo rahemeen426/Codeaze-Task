da71a8f2e58b4c7e90ae2212e317ed8fef9cf840
Rahemeen Khan <128471506+rahemeen426@users.noreply.github.com>
2023-07-07 00:52:32 +0500

    Update Selenium1.py

--- Selenium Automation/Selenium1.py
+++ Selenium Automation/Selenium1.py
@@ -1,1 +1,9 @@
-
+from selenium import webdriver
+# Initialize the web driver
+driver = webdriver.chrome.webdriver.WebDriver()  # Replace with the appropriate driver (e.g., Firefox)
+# Open a website
+driver.get("https://www.example.com")
+# Perform actions or assertions
+print(driver.title)
+# Quit the browser
+driver.quit()
